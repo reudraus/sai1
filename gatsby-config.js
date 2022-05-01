e8b452607d456b075f0fc7b2d8b109e962e503ed
reudraus <80111463+reudraus@users.noreply.github.com>
2022-05-01 17:14:42 +0530

    5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,13 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 module.exports = {
   siteMetadata: {
-    title: `Suntime`,
-    description: `Simple e-commerce training project. JAM Stack: Gatsby.js, DatoCMS and Netlify, Stripe for payments, PWA, and some GSAP fun.`,
-    
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
-
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -18,44 +15,24 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-plugin-snipcart`,
-      options: {
-        apiKey: 'aaaa123',
-        autopop: true,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-playground`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Suntime.com`,
-        short_name: `Suntime`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
-        background_color: `#ffe0c5`,
-        theme_color: `#ffe0c5`,
-        display: `standalone`,
-        icon: `src/images/favicon.png`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-   
-   
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-mailchimp`,
-      options: {
-        endpoint: process.env.MAILCHIMP_ENDPOINT,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-netlify-headers`,
-      options: {
-        allPageHeaders: ['Access-Control-Allow-Origin: *'],
-      },
-    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-};
+}
