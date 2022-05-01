cde30b0b5a74ec2c4ea92a9dbbbdafa5ed729212
reudraus <80111463+reudraus@users.noreply.github.com>
2022-05-01 17:19:54 +0530

    h

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://suntime.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
